7eacedd0c75b617077462756753bb485672ac3dc
vaasuri <varunaasuri@gmail.com>
2022-05-16 23:41:34 -0700

    Minor comment fix.

--- src/main/java/com/aasurihome/springstarter/SpringStarterApplication.java
+++ src/main/java/com/aasurihome/springstarter/SpringStarterApplication.java
@@ -8,14 +8,14 @@ import org.springframework.context.ApplicationContext;
 public class SpringStarterApplication {
 	
 	public static void main(String[] args) {
-		/**
+		/*
 		 * SpringApplication#run bootstraps a spring application as a stand-alone application
 		 * from the main method. It creates an appropriate ApplicationContext instance and
 		 * loads beans. It also runs embedded TomCat Server in a Spring web application.
 		 */
 		ApplicationContext context = SpringApplication.run(SpringStarterApplication.class, args);
 		
-		/**
+		/*
 		 * Here is an example where we fetch an instance of HelloWorld using the ApplicationContext object.
 		 */
 		HelloWorld helloWorld = context.getBean(HelloWorld.class);
